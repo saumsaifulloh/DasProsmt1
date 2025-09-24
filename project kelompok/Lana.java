120fdfc25a081cba7906290550b678a87caee4d3
Saum saifulloh <saummm18@gmail.com>
2025-09-24 16:46:53 +0700

    commit

--- project kelompok/Lana.java
+++
@@ -1,7 +0,0 @@
-public class Lana {
- public static void main(String[] args) {
-    System.out.println("Hallo, nama saya Lana");
-    System.out.println("SALAM KENALAN YA!" );
-    System.out.println("Saya suka banget sama nasi padang");
- }   
-}
--- project kelompok/ProjectKelompok01_02_18.java
+++ project kelompok/ProjectKelompok01_02_18.java
@@ -2,7 +2,7 @@ public class ProjectKelompok01_02_18 {
 public static void main(String[] aStrings) {
     System.out.println("Hallo, nama saya Saum");
     System.out.println("Hallo, nama saya Adzriel");
-    System.out.println("Hallo, nama saya Robby alias ambon");
+    System.out.println("Hallo, nama saya Lana");
     
 }
 }
