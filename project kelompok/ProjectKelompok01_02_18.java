292116e2418007a8556d6246b7542953dc3ba5b6
adzrielllag-rgb <adzrielllag@gmail.com>
2025-09-24 15:42:51 +0700

    first commit ku

--- project kelompok/ProjectKelompok01_02_18.java
+++ project kelompok/ProjectKelompok01_02_18.java
@@ -1,7 +1,8 @@
 public class ProjectKelompok01_02_18 {
 public static void main(String[] aStrings) {
     System.out.println("Hallo, nama saya Saum");
-
-}
+    System.out.println("Hallo, nama saya Adzriel");
     
 }
+}
+    
