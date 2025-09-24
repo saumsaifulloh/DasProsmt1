f0600e46b343ec0bde678dbd1396a39f6ed800bc
lanabuwaik <lanabuwaik@gmail.com>
2025-09-24 16:26:46 +0700

    ubah nasi padang menjadi nasi goreng

--- project kelompok/Lana.java
+++ project kelompok/Lana.java
@@ -2,6 +2,6 @@ public class Lana {
  public static void main(String[] args) {
     System.out.println("Hallo, nama saya Lana");
     System.out.println("SALAM KENALAN YA!" );
-    System.out.println("Saya suka banget sama nasi padang");
+    System.out.println("Saya suka banget sama nasi goreng");
  }   
 }
