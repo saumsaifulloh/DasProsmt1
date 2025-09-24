9610b69634bb5b232d0be3001576f6c02bc3c35a
lanabuwaik <lanabuwaik@gmail.com>
2025-09-24 16:16:58 +0700

    halp

---
+++ project kelompok/Lana.java
@@ -0,0 +1,7 @@
+public class Lana {
+ public static void main(String[] args) {
+    System.out.println("Hallo, nama saya Lana");
+    System.out.println("SALAM KENALAN YA!" );
+    System.out.println("Saya cinta banget sama fatin");
+ }   
+}
