5d55fc400c432922d09421aa4c09f94dc15a6f89
adzrielllag-rgb <adzrielllag@gmail.com>
2025-09-24 15:47:03 +0700

    kerja kelompok bersama ku

--- project kelompok/ProjectKelompok01_02_18.java
+++ project kelompok/ProjectKelompok01_02_18.java
@@ -2,6 +2,7 @@ public class ProjectKelompok01_02_18 {
 public static void main(String[] aStrings) {
     System.out.println("Hallo, nama saya Saum");
     System.out.println("Hallo, nama saya Adzriel");
+    System.out.println("Hallo, nama saya Robby alias ambon");
     
 }
 }
