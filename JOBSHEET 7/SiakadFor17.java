cc2a68f7df179a9c928af67788f83d708ce4a827
Saum saifulloh <saummm18@gmail.com>
2025-10-22 11:00:41 +0700

    JOBSHEET 7

---
+++ JOBSHEET 7/SiakadFor17.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SiakadFor17 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       double nilai, tertinggi = 0, terendah = 100;
+       for (int i = 1; i <= 10; i++) {
+            System.out.print("Masukkan nilai ke-" + i + ": ");
+            nilai = sc.nextDouble();
+           if (nilai > tertinggi) {
+               tertinggi = nilai;
+           }   
+           if (nilai < terendah) {
+               terendah = nilai;
+           }
+       }
+         System.out.println("Nilai tertinggi: " + tertinggi);
+            System.out.println("Nilai terendah: " + terendah);
+    }
+}
